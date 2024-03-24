efb159c24b00ab15ff071ed5b9636994bae54b63
An4ndK <164648201+An4ndK@users.noreply.github.com>
2024-03-24 09:15:49 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)// we are using nodejs,not broswer
+
+//console.log(3+3) ; console.log("anand")//not readable
+
+console.log(3+3) 
+ console.log("anand")
+
+
+
+ let name="anand"
+ let age= 18
+ let isLoggedIn= false
+
+
+ //number= =>2 to the pwr of 53
+ //bigInt
+// string => ""
+// boolean =>true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Anand");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
