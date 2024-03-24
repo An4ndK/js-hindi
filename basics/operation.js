fe82a51a76a8490bc536a2388a3f61175d74fce6
An4ndK <164648201+An4ndK@users.noreply.github.com>
2024-03-24 10:15:55 +0000

    operation

---
+++ basics/operation.js
@@ -0,0 +1,32 @@
+ let value =3
+ let negValue = -value
+//  console.log(value)
+//  console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**3)
+// console.log(2/3)
+
+let str1 = "hello"
+let str2= " Anand"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" + 2+2)
+// console.log(1+ "2")
+// console.log(1 + 2+"2")
+//js converts the entire statemnt to the datatype coming first
+
+console.log(+true)
+console.log(+"")
+
+
+let num1,num2,nume3
+
+num1=num2=num3=2 + 2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
