776d6449bc1f6fd197fa05e1af47606eb24d5085
An4ndK <164648201+An4ndK@users.noreply.github.com>
2024-03-24 10:28:28 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log( 2!=1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null> 0)
+// console.log(null== 0)
+// console.log(null>= 0)
+//Comparisons convert null into a number treating it as 0 therfore null>=o is true and null>0 is false
+
+console.log(undefined ==0 )
+//it wil always give 0 no matter what 
+
+//coparsion and equality check works diffrente in JS
+
+//=== 
+
+console.log("2" ===2 )
