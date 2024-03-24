bb8b512443838aaa3f357349d0494c05c3519775
An4ndK <164648201+An4ndK@users.noreply.github.com>
2024-03-24 10:16:20 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,40 @@
+let score = null
+
+console.log(typeof score);
+
+let ValueInNumber= Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber)
+
+
+let score1 = "anand"
+
+console.log(typeof score1);
+
+let ValueInNumber1= Number(score1)
+console.log(typeof ValueInNumber1);
+console.log(ValueInNumber1)
+
+
+//"33" => 33
+//"33abc"=> NaN
+// true => 1
+
+let IsLoggedIn= undefined
+
+let ValueinBoolean=Boolean(IsLoggedIn)
+console.log(typeof ValueinBoolean)
+console.log(ValueinBoolean)
+
+// 1 => True
+// "" =>F alse
+// "Anand" => True
+// 1-infinty ==> True
+// null => False
+// undefined => false
+
+let x = 33
+ 
+let ValueinString=String(x)
+console.log(typeof ValueinString)
+console.log(ValueinString)
